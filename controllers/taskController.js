746ea5355e4658900071aae52c8c3d922f506b24
Qoslaye <iamqoslaye@gmail.com>
2024-11-02 19:38:25 +0300

    adding delete tasks

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -74,3 +74,27 @@ exports.updateTask = (req, res) => {
     });
 };
 
+exports.deleteTask = (req, res) => {
+    const form = new IncomingForm();
+    form.parse(req, (err, fields) => {
+        if (err) {
+            res.writeHead(400, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ message: 'Error parsing form data.' }));
+            return;
+        }
+
+        const taskId = parseInt(fields.id, 10);
+        const tasks = readTasksFromFile();
+        const updatedTasks = tasks.filter(task => task.id !== taskId);
+
+        if (tasks.length === updatedTasks.length) {
+            res.writeHead(404, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ message: 'Task not found.' }));
+            return;
+        }
+
+        writeTasksToFile(updatedTasks);
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Task deleted successfully.' }));
+    });
+};
